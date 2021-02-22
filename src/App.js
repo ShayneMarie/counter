7493856f4e30d43f262291786593b7f146d4dafe
ShayneMarie <shayne.marie.ulep@gmail.com>
2021-02-22 08:45:18 +0800

    add cookie

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.scss';
 
-import React, { useState } from 'react';
-import $ from 'jquery';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
@@ -16,34 +18,40 @@ function Example() {
   function buttonClickHandler(setButton, button, label){
     setBtnCount(btnCount + 1)
     setButton(button + 1)
+    localStorage.setItem('btnCount', btnCount + 1)
 
-    if (label === 'increment') 
+    if (label === 'increment') {
       setCount(count + 1);
-    else if (label === 'decrement')
+      localStorage.setItem('incCount', incCount + 1)
+      localStorage.setItem('count', count + 1)
+    }
+    else if (label === 'decrement'){
       setCount(count - 1)
-    else if (label === 'reset')
+      localStorage.setItem('decCount', decCount + 1)
+      localStorage.setItem('count', count - 1)
+    }
+    else if (label === 'reset'){
       setCount(0)
+      localStorage.setItem('rstCount', rstCount + 1)
+      localStorage.setItem('count', 0)
+    }
 
-    let context = this;
-
-    $.ajax({
-      url: "http://localhost:3000/api/v1/counter/save",
-      method: "POST",
-      data: {
-        count: context.state.count,
-        btnCount: context.state.btnCount,
-        rstCount: context.state.rstCount,
-        incCount: context.state.incCount,
-        decCount: context.state.decCount
-      },
-      success: function(response) {
-        alert("Successfully saved record!");
-      },
-      error: function(response) {
-        alert("Error in saving record!");
-      }
-    });
+    cookies.set("counter", "counterId", {path: '/', maxAge: 10000000})
+    console.log(cookies.get('counter'))
   }
+
+  useEffect(() => {
+    const initialBtnCount = Number(localStorage.getItem('btnCount'))
+    const initialCount = Number(localStorage.getItem('count'))
+    const initialIncCount = Number(localStorage.getItem('incCount'))
+    const initialDecCount = Number(localStorage.getItem('decCount'))
+    const initialRstCount = Number(localStorage.getItem('rstCount'))
+    if (initialBtnCount) setBtnCount(initialBtnCount)
+    if (initialCount) setCount(initialCount)
+    if (initialIncCount) setIncCount(initialIncCount)
+    if (initialDecCount) setDecCount(initialDecCount)
+    if (initialRstCount) setRstCount(initialRstCount)
+  }, []);
 
   return (
     <div className='counter'>
